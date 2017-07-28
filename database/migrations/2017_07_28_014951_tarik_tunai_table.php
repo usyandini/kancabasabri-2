b1c66cb5e62a2b90a85ea5a49e3b9670a63ff8f8
usyandini <usyandini04@gmail.com>
2017-07-28 10:45:02 +0700

    add tarik tunai migrations

---
+++ database/migrations/2017_07_28_014951_tarik_tunai_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TarikTunaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarik_tunai', function (Blueprint $table) {
+            $table->increments('id_tunai')->unique();
+            $table->date('tgl_dopping');
+            $table->double('nominal_tunai');
+            $table->string('akun_bank',50);
+            $table->string('rek_bank',50);
+            $table->string('cabang',50);
+            $table->string('status',50); 
+            $table->double('ket_nominal')->nullable();
+            /*$table->string('ket_bank',50)->nullable();
+            $table->string('ket_rek',50)->nullable();
+            $table->string('ket_cabang',50)->nullable();*/
+            $table->timestamps();       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tarik_tunai');
+    }
+}
