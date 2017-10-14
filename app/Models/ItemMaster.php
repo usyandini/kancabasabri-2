0f3f43279bfd3c29d93e903a0346501983df3dac
Ilyas <ilyashabiburrahman@gmail.com>
2017-10-14 19:45:48 +0700

    Add flag to item barang/jasa

--- app/Models/ItemMaster.php
+++ app/Models/ItemMaster.php
@@ -9,7 +9,7 @@ class ItemMaster extends Model
     protected $connection = 'sqlsrv';
     protected $table = 'item_master';
 
-    // protected $dateFormat = 'Y-m-d H:i:s';
+    protected $dateFormat = 'Y-m-d H:i:s';
 
     protected $dates = ['dob'];
     
@@ -44,6 +44,15 @@ class ItemMaster extends Model
     public function isAxAnggaranAvailable($transaksi_date)
     {
         return $this->axAnggaran($transaksi_date) ? true : false;   
+    }
+
+    public function isDisplayed($cabang)
+    {
+        if ($this->is_displayed == "0") {
+            return $this->SEGMEN_3 == $cabang;
+        }  
+
+        return true;
     }
 
     public function budgetHistory($transaksi_date)
