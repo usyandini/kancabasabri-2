be56842ac6fe85b62ff64d7f7ea67f073ba11f7e
usyandini <usyandini04@gmail.com>
2017-10-16 13:33:18 +0700

    template report kas/bank transaksi

---
+++ resources/views/transaksi/cetak-kasbank.blade.php
@@ -0,0 +1,108 @@
+<html>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<style>
+		h3 {
+            text-transform: uppercase;
+        }
+		table{
+			border-collapse:collapse;
+			
+			margin-left:auto;
+			margin-right:auto;
+		}
+		td, th{
+        	border: 1px solid #000;
+        	padding: 5px;	
+		}
+        th {
+            background-color: #cccccc;
+        }
+        td{
+        	border-bottom: 1px solid #000;
+            font-size: 75%;
+        }
+	</style>
+<body>
+	<div id="header">
+		<img src='<?php echo $_SERVER["DOCUMENT_ROOT"].'/app-assets/images/asabri-logo.png'; ?>' align="left" style="max-width: 132px;">
+        <div="title">
+	        <h3><center>LAPORAN KAS & BANK PT  ASABRI (PERSERO)</center></h3>
+	        <h3><center>KANTOR CABANG BENGKULU</center></h3>
+	        <h3><center>BULAN AGUSTUS TAHUN 2017</center></h3>
+	    </div>
+    </div>
+    <br><br>
+    <div id="content">
+    	<div style="overflow-x:auto;">
+	    	<table>
+	    		<thead>
+	              <tr>
+	                <th rowspan="2" width="60px"><center>TGL</center></th>
+	                <th rowspan="2" width="50px"><center>NO.BK</center></th>
+	                <th rowspan="2" colspan="2"><center>URAIAN TRANSAKSI</center></th>
+	                <th colspan="2" width="100px"><center>KAS</center></th>
+	                <th colspan="2" width="100px"><center>BANK</center></th>
+	                <th rowspan="2" width="80px"><center>SALDO</center></th>
+	              </tr>
+	              <tr>
+	                <th><center>DEBET</center></th>
+	                <th><center>KREDIT</center></th>
+	                <th><center>DEBET</center></th>
+	                <th><center>KREDIT</center></th>
+	              </tr>
+	              <tr>
+	                <th><center>1</center></th>
+	                <th><center>2</center></th>
+	                <th colspan="2"><center>3</center></th>
+	                <th><center>4</center></th>
+	                <th><center>5</center></th>
+	                <th><center>6</center></th>
+	                <th><center>7</center></th>
+	                <th><center>8</center></th>
+	              </tr>
+	            </thead>
+	            <tbody>
+	            	<tr id="th1">
+	            		<td colspan="4"><center><b>SALDO PER 31 Juli 2017</b></center></td>
+		                <td align="right"><?php $debitkas = 1912862; echo number_format($debitkas,'2',',','.'); ?></td>
+		                <td align="right"></td>
+		                <td align="right"><?php $debitbank = 37910743.8; echo number_format($debitbank,'2',',','.'); ?></td>
+		                <td align="right"></td>
+		                <td align="right"><?php $saldo = $debitkas+$debitbank; echo number_format($saldo,'2',',','.'); ?></td>
+	            	</tr>
+	            	<tr id"content">
+	            		<td></td>
+		                <td></td>
+		                <td width="150px"></td>
+		                <td width="80px"></td>
+		                <td align="right"></td>
+		                <td align="right"></td>
+		                <td align="right"></td>
+		                <td align="right"></td>
+		                <td align="right"><?php $saldo = $debitkas+$debitbank; echo number_format($saldo,'2',',','.'); ?></td>
+	            	</tr>
+	            	<tr id"content">
+	            		<td></td>
+		                <td></td>
+		                <td width="150px"></td>
+		                <td width="80px"></td>
+		                <td align="right"></td>
+		                <td align="right"></td>
+		                <td align="right"></td>
+		                <td align="right"></td>
+		                <td align="right"><?php $saldo = $debitkas+$debitbank; echo number_format($saldo,'2',',','.'); ?></td>
+	            	</tr>
+	            	<tr id="tf1">
+	            		<td colspan="4"><center><b>JUMLAH</b></center></td>
+		                <td align="right"><?php $debitkas = 1912862; echo number_format($debitkas,'2',',','.'); ?></td>
+		                <td align="right">-</td>
+		                <td align="right"><?php $debitbank = 37910743.8; echo number_format($debitbank,'2',',','.'); ?></td>
+		                <td align="right">-</td>
+		                <td align="right"><?php $saldo = $saldo; echo number_format($saldo,'2',',','.'); ?></td>
+	            	</tr>
+	            </tbody>	            
+	    	</table>
+	    </div>
+    </div>
+</body>
+<html>
