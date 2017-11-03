db229edd198fd79399c65c7e11c20cabd16ce9fc
Mohammad Toriq <toriq031194@gmail.com>
2017-11-03 14:16:29 +0700

    perbaikan import item kombinasi transaksi

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -978,7 +978,7 @@ class ItemController extends Controller
                 $kode = '';
                 if(count($item_master) == 0){
                     $kode = 'KD-'.(count($item_master)+1);
-                    ItemAnggaranMaster::create($inputJenis);
+                    ItemAnggaranMaster::create($item_master);
                 }else{
                     foreach ($item_master as $row) {
                         $kode = $row->kode_item;
