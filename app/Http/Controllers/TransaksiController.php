6eb643cc7acedd84aa53cc6499fd97a2f3dc19a7
unknown <Hutomo@Geforce-PC.asabri.co.id>
2018-02-21 14:59:10 +0700

    pembuatan menu reverse transaksi 5

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -593,7 +593,7 @@ class TransaksiController extends Controller
                 'PIL_PROGRAM'           => $list->PIL_PROGRAM,
                 'PIL_SUBPOS'            => $list->PIL_SUBPOS,
                 'PIL_TRANSDATE'         => $list->PIL_TRANSDATE,
-                'PIL_TXT'               => $list->PIL_TXT.'(REVERSE)',
+                'PIL_TXT'               => $list->PIL_TXT.' (REVERSED)',
                 'PIL_VOUCHER'           => $list->PIL_VOUCHER,
                 'PIL_KCJOURNALNUM'      => $list->PIL_KCJOURNALNUM,
                 'DATAAREAID'            => $list->DATAAREAID,
